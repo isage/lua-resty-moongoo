506b4300a8b45b531fd423d2d16b61d8b4461eaf
ufownl <ufownl@gmail.com>
2019-06-19 13:02:49 +0800

    Fix the remove method of GridFS according the documents

--- lib/resty/moongoo/gridfs.lua
+++ lib/resty/moongoo/gridfs.lua
@@ -22,11 +22,11 @@ function _M.list(self)
 end
 
 function _M.remove(self, id)
-  local r,err = self._files:remove({_id = cbson.oid(id)})
+  local r,err = self._files:remove({_id = id})
   if not r then
     return nil, "Failed to remove file metadata: "..err
   end
-  r,err = self._chunks:remove({files_id = cbson.oid(id)});
+  r,err = self._chunks:remove({files_id = id});
   if not r then
     return nil, "Failed to remove file chunks: "..err
   end
